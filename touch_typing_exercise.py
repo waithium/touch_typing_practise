7b279c63a09a72d998076a1657adc42acbe64bf7
waithium <160033656+waithium@users.noreply.github.com>
2024-03-19 06:15:08 +0530

    Update touch_typing_exercise.py
    added exercise 4

--- touch_typing_exercise.py
+++ touch_typing_exercise.py
@@ -1,10 +1,11 @@
 #touch typing exercise
+import random
 
 #exercise 1
 def exercise_1():
     print('repeat the following letters (don\'t add space after the last letter)')
-    lett = input('a s d f j k l ; \n').lower()
-    if lett == 'a s d f j k l ;':
+    ans = input('a s d f j k l ; \n').lower()
+    if ans == 'a s d f j k l ;':
         print('well done!!')
         while True:
             action = input('try again(a)/ quit(j): ')
@@ -28,8 +29,8 @@ def exercise_1():
 #exercise 2
 def exercise_2():
     print('repeat the following letters (don\'t add space after the last letter)')
-    lett = input('a d s f j l k ; \n').lower()
-    if lett == 'a d s f j l k ;':
+    ans = input('a d s f j l k ; \n').lower()
+    if ans == 'a d s f j l k ;':
         print('well done!!')
         while True:
             action = input('try again(a)/ quit(j): ')
@@ -53,8 +54,8 @@ def exercise_2():
 #exercise 3
 def exercise_3():
     print('repeat the following letters (don\'t add space after the last letter)')
-    lett = input('a f s d j ; k l \n').lower()
-    if lett == 'a f s d j ; k l':
+    ans = input('a f s d j ; k l \n').lower()
+    if ans == 'a f s d j ; k l':
         print('well done!!')
         while True:
             action = input('try again(a)/ quit(j): ')
@@ -75,7 +76,61 @@ def exercise_3():
         else:
             print("wrong ans, try 'y' or 'n': ")
         
-    
+#exercise 4
+def exercise_4():
+    tries = 0
+    while True:
+        rand = random.randint(0, 5)
+        if tries < 4:
+            if rand == 0:
+                print('repeat the following letters (don\'t add space after the last letter)')
+                ans = input('sad')
+                if ans == 'sad':
+                    print('well done')
+                    tries += 1
+                    continue
+                else:
+                    print('wrong ans')
+                    continue
+            elif rand == 1:
+                print('repeat the following letters (don\'t add space after the last letter)')
+                ans = input('dad')
+                if ans == 'dad':
+                    print('well done')
+                    tries += 1
+                    continue
+                else:
+                    print('wrong ans')
+                    continue
+            elif rand == 2:
+                print('repeat the following letters (don\'t add space after the last letter)')
+                ans = input('fad')
+                if ans == 'fad':
+                    print('well done')
+                    tries += 1
+                    continue
+                else:
+                    print('wrong ans')
+                    continue
+            elif rand == 3:
+                print('repeat the following letters (don\'t add space after the last letter)')
+                ans = input('dsf')
+                if ans == 'dsf':
+                    print('well done')
+                    tries += 1
+                    continue
+                else:
+                    print('wrong ans')
+                    continue
+        else:
+            action = input('try again(a)/ quit(j): ')
+            if action == 'a':
+                exercise_4()
+            elif action == 'j':
+                main_menu()
+            else:
+                print('invalid options')
+                continue
 
 
 #main menu
@@ -90,6 +145,8 @@ def main_menu():
         exercise_2()
     elif mode == '3':
         exercise_3()
+    elif mode == '4':
+        exercise_4()
     else:
         print('invalid !!')
 
